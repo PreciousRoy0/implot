75947c408beaab9fd9afbac1f8bd015cf4df608b
PreciousRoy0 <danieisnietgek@hotmail.com>
2022-08-06 20:55:06 +0200

    Added Dist config to premake script

---
+++ premake5.lua
@@ -0,0 +1,38 @@
+project "ImPlot"
+	kind "StaticLib"
+	language "C++"
+    staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"implot.h",
+		"implot.cpp",
+		"implot_demo.cpp",
+		"implot_internal.h",
+		"implot_items.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
